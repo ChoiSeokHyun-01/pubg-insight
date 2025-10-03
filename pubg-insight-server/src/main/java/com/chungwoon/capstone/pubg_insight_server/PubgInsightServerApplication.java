226f0f5c8e20f87e9b776b33848e82c885a2bd28
ohjonghyuk <ohjonghyuk0717@gmail.com>
2025-10-03 15:29:02 +0900

    백 메이븐 프로젝트 그레이들로 변경

---
+++ pubg-insight-server/src/main/java/com/chungwoon/capstone/pubg_insight_server/PubgInsightServerApplication.java
@@ -0,0 +1,13 @@
+package com.chungwoon.capstone.pubg_insight_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgInsightServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubgInsightServerApplication.class, args);
+	}
+
+}
