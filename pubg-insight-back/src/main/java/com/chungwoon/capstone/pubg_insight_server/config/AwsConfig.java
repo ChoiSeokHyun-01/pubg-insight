ea00d32ebce23adac0a7befadfdfce99b79d4606
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-25 22:27:10 +0900

    feat: S3에 연결할 설정 추가

---
+++ pubg-insight-back/src/main/java/com/chungwoon/capstone/pubg_insight_server/config/AwsConfig.java
@@ -0,0 +1,16 @@
+package com.chungwoon.capstone.pubg_insight_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class AwsConfig {
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2) // 서울
+                .build();
+    }
+}
