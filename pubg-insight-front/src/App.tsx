86c8c6f928f07e1e7423cf709997e61edbfbff37
ChoiSeokHyun-01 <chltjrgus202@naver.com>
2025-11-27 01:32:41 +0900

    PlayerSearch 요청 테스트 임시 페이지

--- pubg-insight-front/src/App.tsx
+++ pubg-insight-front/src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/App.css";
 import HealthCheck from "./pages/HealthCheck";
 import MapPage from "./pages/Map";
 import Home from "./pages/Home";
+import PlayerSearch from "./pages/tmp/PlayerSearch";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/map/:name" element={<MapPage />}></Route>
           <Route path="/map" element={<MapPage />}></Route>
+          <Route path="/PlayerSearch" element={<PlayerSearch></PlayerSearch>}></Route>  
           <Route path="/HealthCheck" element={<HealthCheck></HealthCheck>}></Route>  
         </Routes>
       </BrowserRouter>
--- pubg-insight-front/src/pages/Home.tsx
+++ pubg-insight-front/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Button from "../components/Button";
 export default function Home() {
     return <>
         <Button to={"/map/sanhok"} >to sanhok</Button>
+        <Button to="/PlayerSearch" replace>PlayerSearch</Button>
         <Button to="/HealthCheck" replace>Health</Button>
     </>
 }
