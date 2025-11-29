70144983caea4f83c0ce8e6bd0e74c25c92d86b1
ChoiSeokHyun-01 <chltjrgus202@naver.com>
2025-11-29 21:18:20 +0900

    정리

--- pubg-insight-front/src/App.tsx
+++ pubg-insight-front/src/App.tsx
@@ -1,37 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./styles/App.css";
-import HealthCheck from "./pages/HealthCheck";
 import MapPage from "./pages/Map";
-import Home from "./pages/Home";
-import PlayerSearch from "./pages/tmp/PlayerSearch";
-
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import PlayerOverviewPage from "./pages/PlayerOverniewPage";
-import SearchHome from "./pages/SearchHome";
+import './styles/app.css'
 
 export default function App() {
     return (
         <>
-            <Header />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home></Home>}></Route>
+                    <Route path="/" element={<></>}></Route>
                     <Route path="/map/:name" element={<MapPage />}></Route>
                     <Route path="/map" element={<MapPage />}></Route>
-                    <Route
-                        path="/PlayerSearch"
-                        element={<PlayerSearch></PlayerSearch>}
-                    ></Route>
-                    <Route
-                        path="/HealthCheck"
-                        element={<HealthCheck></HealthCheck>}
-                    ></Route>
-                    <Route path="/search-home" element={<SearchHome />}></Route>
-                    <Route path="/match-history" element={<PlayerOverviewPage />}></Route>
                 </Routes>
             </BrowserRouter>
-            <Footer />
         </>
     );
 }
