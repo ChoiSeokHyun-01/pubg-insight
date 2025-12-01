5d283666b6d32e5d706bfa0f3b2b3777786ef21e
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-12-01 22:14:01 +0900

    feat: 마지막 전적갱신 시간 전적갱신응답데이터에 추가

--- pubg-insight-back/src/main/java/com/chungwoon/capstone/pubg_insight_server/historyrefresh/dto/HistoryRefreshResponse.java
+++ pubg-insight-back/src/main/java/com/chungwoon/capstone/pubg_insight_server/historyrefresh/dto/HistoryRefreshResponse.java
@@ -5,9 +5,12 @@ import com.chungwoon.capstone.pubg_insight_server.rankstats.DTO.RankStatsBundle;
 import com.chungwoon.capstone.pubg_insight_server.seasonstats.dto.SeasonStatsBundle;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
+
 public record HistoryRefreshResponse(
         ResponseEntity<SeasonStatsBundle> seasonStatsBundle,
         ResponseEntity<RankStatsBundle> rankStatsBundle,
-        MatchResponse matchResponse
+        MatchResponse matchResponse,
+        LocalDateTime lastRefreshedAt
 ) {
 }
